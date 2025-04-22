4774ef533e03a816b6d4da9a54b188369c3fb7f8
Nick Meinhold <nick@enspyr.co>
2025-04-22 19:28:47 +1000

    Add cloud_firestore and a query

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String _outputString = '';
+
+  Future<void> _retrieveDocs() async {
+    final QuerySnapshot<Map<String, dynamic>> querySnapshot =
+        await FirebaseFirestore.instance
+            .collection('collection')
+            .where(FieldPath.documentId, whereIn: ['a', 'set', 'of', 'ids'])
+            .get();
+
+    for (final doc in querySnapshot.docs) {
+      _outputString += doc['text'] + ' ';
+    }
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _retrieveDocs();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text(_outputString))),
     );
   }
 }
